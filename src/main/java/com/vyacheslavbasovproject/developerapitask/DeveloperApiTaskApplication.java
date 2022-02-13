f3238d69da9ad4f4a77f002c7110fb0a01123751
Vyacheslav Basov <slavabasov987@gmail.com>
2022-02-13 14:57:05 +0300

    first commit

---
+++ src/main/java/com/vyacheslavbasovproject/developerapitask/DeveloperApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vyacheslavbasovproject.developerapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperApiTaskApplication.class, args);
+    }
+
+}
