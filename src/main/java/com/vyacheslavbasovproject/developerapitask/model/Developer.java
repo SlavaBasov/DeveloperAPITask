d62f9bcb5768685dfe0e933999e42a02cbdb0641
Vyacheslav Basov <slavabasov987@gmail.com>
2022-02-13 16:06:19 +0300

    creating Developer.class

---
+++ src/main/java/com/vyacheslavbasovproject/developerapitask/model/Developer.java
@@ -0,0 +1,42 @@
+package com.vyacheslavbasovproject.developerapitask.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "developers")
+@Getter @Setter @NoArgsConstructor @ToString
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public Developer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Developer developer = (Developer) o;
+        return id != null && Objects.equals(id, developer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
