d64162c29ae1d2bdb8d1e5b5b4e0858423708094
Vyacheslav Basov <slavabasov987@gmail.com>
2022-02-16 20:04:25 +0300

    Working app without tests

--- src/main/java/com/vyacheslavbasovproject/developerapitask/controller/exceptionsHandling/CustomErrorResponse.java
+++ src/main/java/com/vyacheslavbasovproject/developerapitask/controller/exceptionsHandling/CustomErrorResponse.java
@@ -1,9 +1,11 @@
 package com.vyacheslavbasovproject.developerapitask.controller.exceptionsHandling;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class CustomErrorResponse {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
@@ -11,27 +13,4 @@ public class CustomErrorResponse {
     private int status;
     private String error;
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
 }
